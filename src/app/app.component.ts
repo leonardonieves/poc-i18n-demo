518415011d7de6770d659e01cb953858db294e36
Leonardo Nieves <leonardo.nieves@ntsprint.com>
2025-05-03 23:28:55 -0400

    Add material styles and dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
+  currentLang = 'en';
+  isDarkMode = false;
+
+  constructor(
+    private translate: TranslateService,
+    private renderer: Renderer2  // ✅ ahora sí Renderer2 está inyectado
+  ) {
     translate.addLangs(['en', 'es']);
-    translate.setDefaultLang('es');
-    translate.use('es');
+    translate.setDefaultLang('en');
+    translate.use('en');
   }
 
   switchLanguage(lang: string) {
+    this.currentLang = lang;
     this.translate.use(lang);
   }
+
   onLanguageChange(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     const lang = selectElement.value;
     this.switchLanguage(lang);
   }
-  
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark-theme');
+    } else {
+      this.renderer.removeClass(document.body, 'dark-theme');
+    }
+  }
 }
